0cfda2ca5a47d476a1ae6471e93501703195d1f7
Allan Zhou <allanlinzhou@gmail.com>
2023-06-05 10:53:22 -0400

    Small server modification

--- ClientTicTacToe.java
+++ ClientTicTacToe.java
@@ -3,35 +3,44 @@ import java.awt.*;
 
 public class ClientTicTacToe extends JFrame {
     public static void main(String[] args) {
-	JFrame mainWindow = new JFrame(); 
-	
-	mainWindow.setTitle("Allan Zhou");
-	mainWindow.setLocation(Globals.FRAME_X, Globals.FRAME_Y);
-	mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); 
-	mainWindow.setResizable(false); 
-	
-	JPanel tttPanel = new JPanel(); 
-	tttPanel.setLayout(new GridLayout(Globals.ROWS, Globals.COLS));
-	Color c = new Color(176, 224, 230); 
+	Globals.serverIPAddress = Utils.initialNetworkConnection();
 
-	for (int row = 0; row < Globals.ROWS; row++) {
-		for (int col = 0; col < Globals.COLS; col++) {
-			Globals.grid[row][col] = new GridPanel(c, row, col, Globals.NO_PLAYER);
-			tttPanel.add(Globals.grid[row][col]); 
+	if (Globals.serverIPAddress != null) {
+		JFrame mainWindow = new JFrame(); 
+	
+		mainWindow.setTitle("Allan Zhou");
+		mainWindow.setLocation(Globals.FRAME_X, Globals.FRAME_Y);
+		mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); 
+		mainWindow.setResizable(false); 
+		
+		JPanel tttPanel = new JPanel(); 
+		tttPanel.setLayout(new GridLayout(Globals.ROWS, Globals.COLS));
+		Color c = new Color(176, 224, 230); 
+	
+		for (int row = 0; row < Globals.ROWS; row++) {
+			for (int col = 0; col < Globals.COLS; col++) {
+				Globals.grid[row][col] = new GridPanel(c, row, col, Globals.NO_PLAYER);
+				tttPanel.add(Globals.grid[row][col]); 
+			}
 		}
-	}
+		
+		GridBagLayout gridBag = new GridBagLayout(); 
+		mainWindow.getContentPane().setLayout(gridBag); 
+
+		Globals.gridBagConstraints.gridheight = Globals.ROWS; 
+		mainWindow.getContentPane().add(tttPanel, Globals.gridBagConstraints); 
 	
-	GridBagLayout gridBag = new GridBagLayout(); 
-	mainWindow.getContentPane().setLayout(gridBag); 
-
-	Globals.gridBagConstraints.gridx = 0; 
-	Globals.gridBagConstraints.gridy = Globals.ROWS; 
-	Globals.gridBagConstraints.anchor = GridBagConstraints.LINE_START; 
-	mainWindow.getContentPane().add(Globals.status, Globals.gridBagConstraints);
-
-	mainWindow.getContentPane().add(tttPanel); 
-	mainWindow.pack();
-	mainWindow.setVisible(true);  
+		Globals.gridBagConstraints.gridx = 0; 
+		Globals.gridBagConstraints.gridy = Globals.ROWS; 
+		Globals.gridBagConstraints.anchor = GridBagConstraints.LINE_START;
+		
+		mainWindow.getContentPane().add(Globals.status, Globals.gridBagConstraints);
+		
+		mainWindow.pack();
+		mainWindow.setVisible(true);  
+	}
+	else {
+		System.out.println("No server selected");
+	}
     } 
-
 }
